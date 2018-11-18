2d7e6e6c2200243fb765fb2f83c48efefed4293c
Alec Rubin <alecrubindev@gmail.com>
2018-11-18 15:19:58 -0700

    updates

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Flask is Dockerized!'
+    env = request.environ
+    return f"Flask is Dockerized! @ {env['REMOTE_ADDR']}:{env['REMOTE_PORT']}"
 
 
 if __name__ == '__main__':
